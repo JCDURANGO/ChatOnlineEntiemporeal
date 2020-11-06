6091d6f7927890953dfb624b74e02a606519bad2
JCDURANGO <71443849+JCDURANGO@users.noreply.github.com>
2020-11-06 15:20:57 -0500

    Agregando comandos
    comandos necesarios para ejecutar nodemon, y estableciendo http

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const app = require("express")();
     const http = require("http").Server(app);
+    const io = require("socket.io")(http);
    
     app.get("/", function(req, res) {
         res.sendFile(__dirname + "/index.html");

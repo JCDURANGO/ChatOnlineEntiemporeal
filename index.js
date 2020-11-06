13c7859374bac297e15d8f3651b9a3be7a70c5e3
Normi41 <jjuliomontiel01@correo.unicordona.edu.co>
2020-11-06 14:39:11 -0500

    Estableciendo comunicación entre clientes

--- index.js
+++ index.js
@@ -4,6 +4,26 @@ const app = require("express")();
     app.get("/", function(req, res) {
         res.sendFile(__dirname + "/index.html");
     });
+    
+    io.on("connection", function(socket) {
+        console.log('Nuevo cliente se ha conectado');
+        
+        socket.on("user_join", function(data) {
+            this.username = data;
+            socket.broadcast.emit("user_join", data);
+        });
+
+        socket.on("chat_message", function(data) {
+            data.username = this.username;
+            socket.broadcast.emit("chat_message", data);
+            console.log( 'message: ' + data.message);
+           
+        });
+
+        socket.on("disconnect", function(data) {
+            socket.broadcast.emit("user_leave", this.username);
+        });
+    });
 
         http.listen(3000, () => {
             console.log('listening on *:3000');
